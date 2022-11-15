9f78be59a1ca0abe640f2c483fbb872c6173390a
sztivi01 <56344812+sztivi01@users.noreply.github.com>
2022-11-15 19:53:00 +0100

    start of profile page

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <div>
+            <img src="" alt="pic" />
+            <h5>Someone famous</h5>
+            <h5>Email - test@test.com</h5>
+            <h5>Handle of someone famous</h5>
+        </div>
+        <blockquote>
+            <h6>if it works dont touch it</h6>
+            <p>ram</p>
+        </blockquote>
+    </div>
+  )
+}
