9c3d3edc3c6a6cb4a78841690aa25875a8b21d5f
Daniel Radosa <danysko5@gmail.com>
2022-11-22 18:24:51 +0100

    somewhat working updates

--- src/components/profileSettings.jsx
+++ src/components/profileSettings.jsx
@@ -1,131 +1,135 @@
 import React, { useCallback } from "react";
-import { useMutation } from "@apollo/client";
-import { UPDATE_PROFILE } from "../utils/Mutations";
+import { useMutation, useQuery } from "@apollo/client";
+import { UPDATE_USERNAME, UPDATE_EMAIL } from "../utils/Mutations";
+import { GET_CURRENT_USER } from "../utils/Queries";
 
 export default function Profile() {
-  const user = JSON.parse(localStorage.getItem("currentUser") || sessionStorage.getItem("currentUser"));
+  const { loading, data } = useQuery(GET_CURRENT_USER);
+  const user = JSON.parse(
+    localStorage.getItem("currentUser") || sessionStorage.getItem("currentUser")
+  );
 
-  const [activeEditField, setActiveEditField] = React.useState(''); //Push the field user wants to edit
+  const [activeEditField, setActiveEditField] = React.useState(""); //Push the field user wants to edit
 
-  const [username, setUsername] = React.useState(user.username || '');
-  const [userEmail, setUserEmail] = React.useState(user.email || '');
-  const [userBio, setUserBio] = React.useState(user.bio || '');//bio does not exist in the local/session storage
-  const [userHandle, setUserHandle] = React.useState(user.handle || '');
-  const [userProfilePicture, setUserProfilePicture] = React.useState('something' || '');
+  const [username, setUsername] = React.useState(user.username || "");
+  const [userEmail, setUserEmail] = React.useState(user.email || "");
 
-  const [editUser] = useMutation(UPDATE_PROFILE);
+  const [updateUsername] = useMutation(
+    UPDATE_USERNAME, { refetchQueries: ["GET_CURRENT_USER"] } 
+  );
+  const [updateEmail] = useMutation(
+    UPDATE_EMAIL, { refetchQueries: ["GET_CURRENT_USER"] }
+  );
 
   const handleSubmit = useCallback(
     (e) => {
       e.preventDefault();
-
-      const bio = {
-        bioBy: 'this is string',
-        body: 'this is string',
-        website: 'this is string',
-        location: 'this is string',
+      if (activeEditField === "username") {
+        updateUsername({
+          variables: {
+            id: user.id,
+            username: username,
+            token: user.token,
+          },
+        });
+      } else if (activeEditField === "email") {
+        updateEmail({
+          variables: {
+            id: user.id,
+            email: userEmail,
+            token: user.token,
+          },
+        });
       }
-      editUser({
-        variables: { username, email: userEmail, handle: userHandle, profilePicture: 'nope', bio: bio },
-      }).then(x => { console.log('update', x) }).catch(error => { console.log('updateeerror', error) });
-
     },
-    [username, userEmail, userHandle,]
-  )
+    [activeEditField, updateUsername, updateEmail, user, username, userEmail]
+  );
 
   return (
-    <div class="overflow-hidden bg-white shadow sm:rounded-lg">
-      <div class="px-4 py-5 sm:px-6">
-        <h3 class="text-lg font-medium leading-6 text-gray-900">Placeholder for picture</h3>
+    <div className="overflow-hidden bg-white shadow-lg mt-8 w-1/3 float-left sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg font-medium leading-6 text-gray-900">
+          Placeholder for picture
+        </h3>
       </div>
-      <div class="border-t border-gray-200">
+      <div className="border-t border-gray-200">
         <dl>
-
-          {activeEditField == 'username' ? (
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Username</dt>
+          {activeEditField == "username" ? (
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Username</dt>
               <form action="">
                 <input
                   type="text"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                 />
-                <button type="submit" onClick={handleSubmit} >
+                <button type="submit" onClick={handleSubmit}>
                   Save
                 </button>
-                <button onClick={() => setActiveEditField("")}>
-                  Cancel
-                </button>
+                <button onClick={() => setActiveEditField("")}>Cancel</button>
               </form>
             </div>
-          ) :
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6" onClick={() => setActiveEditField("username")}>
-              <dt class="text-sm font-medium text-gray-500">Username</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">{username}</dd>
-              <button class="sm:col-span-1 text-right" onClick={() => setActiveEditField("username")} >Edit</button>
+          ) : (
+            <div
+              className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+              onClick={() => setActiveEditField("username")}
+            >
+              <dt className="text-sm font-medium text-gray-500">Username</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">
+                {username}
+              </dd>
+              <button
+                className="sm:col-span-1 text-right"
+                onClick={() => setActiveEditField("username")}
+              >
+                Edit
+              </button>
             </div>
-          }
+          )}
 
-          {/*           {activeEditField == 'userEmail' ? (
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Email Address</dt>
+          {activeEditField == "email" ? (
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Email</dt>
               <form action="">
                 <input
                   type="text"
                   value={userEmail}
                   onChange={(e) => setUserEmail(e.target.value)}
                 />
-                <button type="submit" >
+                <button type="submit" onClick={handleSubmit}>
                   Save
                 </button>
-                <button onClick={() => setActiveEditField("")}>
-                  Cancel
-                </button>
+                <button onClick={() => setActiveEditField("")}>Cancel</button>
               </form>
-
             </div>
-          ) :
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6" onClick={() => setActiveEditField("userEmail")}>
-              <dt class="text-sm font-medium text-gray-500">Email Address</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">{userEmail}</dd>
-              <button class="sm:col-span-1 text-right" onClick={() => setActiveEditField("userEmail")} >Edit</button>
+          ) : (
+            <div
+              className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+              onClick={() => setActiveEditField("email")}
+            >
+              <dt className="text-sm font-medium text-gray-500">Email</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">
+                {userEmail}
+              </dd>
+              <button
+                className="sm:col-span-1 text-right"
+                onClick={() => setActiveEditField("email")}
+              >
+                Edit
+              </button>
             </div>
-          } */}
+          )}
+          
 
-          {/* {activeEditField == 'userHandle' ? (
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Handle</dt>
-              <form action="">
-                <input
-                  type="text"
-                  value={userHandle}
-                  onChange={(e) => setUserHandle(e.target.value)}
-                />
-                <button type="submit" >
-                  Save
-                </button>
-                <button onClick={() => setActiveEditField("")}>
-                  Cancel
-                </button>
-              </form>
-
-            </div>
-          ) :
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6" onClick={() => setActiveEditField("userHandle")}>
-              <dt class="text-sm font-medium text-gray-500">Handle</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">{userHandle}</dd>
-              <button class="sm:col-span-1 text-right" onClick={() => setActiveEditField("userHandle")} >Edit</button>
-            </div>
-          }
-
-          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">Bio</dt>
-            <dd class="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">{user.bio}</dd>
-            <button class="sm:col-span-1 text-right">Edit</button>
-          </div> */}
-
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Bio</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">
+              {user.bio}
+            </dd>
+            <button className="sm:col-span-1 text-right">Edit</button>
+          </div>
         </dl>
       </div>
     </div>
-  )
+  );
 }
