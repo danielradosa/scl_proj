602053374d0ac0fe824d571e0512ef52c499547d
Daniel Radosa <danysko5@gmail.com>
2022-12-05 10:12:47 +0100

    Update logoutButton.jsx

--- src/components/logoutButton.jsx
+++ src/components/logoutButton.jsx
@@ -1,21 +1,20 @@
 import { useNavigate } from "react-router-dom";
-import React, { useCallback, useState } from "react";
-import { Spinner } from "./Spinner";
+import React, { useCallback } from "react";
 
 const LogoutButton = () => {
   const navigate = useNavigate();
-  const [loggedOutState, setLoggedOutState] = useState(false);
 
   const handleLogout = useCallback(() => {
-    setLoggedOutState(true);
-    localStorage.clear();
-    sessionStorage.clear();
+    // remove token and all user info from local and session storage
+    localStorage.removeItem("token");
+    localStorage.removeItem("currentUser");
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("currentUser");
     navigate("/login", { replace: true });
   }, [navigate]);
 
   return (
     <div className="logout">
-      {loggedOutState === true ? <Spinner /> : ""}
       <button onClick={handleLogout} className="bg-white rounded-lg text-slate-400 pl-4 pr-4 mt-6 mr-6 shadow-sm">
         Logout
       </button>
